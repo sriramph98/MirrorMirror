44e496a821f92941dea2e182f4a98a0fde05ea26
Sriram P H <sriramph9812@gmail.com>
2025-01-11 17:04:11 -0500

    Initial Commit

---
+++ MirrorMirror/MirrorMirrorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MirrorMirrorApp.swift
+//  MirrorMirror
+//
+//  Created by Sriram P H on 1/11/25.
+//
+
+import SwiftUI
+
+@main
+struct MirrorMirrorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MirrorMirrorTests/MirrorMirrorTests.swift
@@ -0,0 +1,17 @@
+//
+//  MirrorMirrorTests.swift
+//  MirrorMirrorTests
+//
+//  Created by Sriram P H on 1/11/25.
+//
+
+import Testing
+@testable import MirrorMirror
+
+struct MirrorMirrorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
